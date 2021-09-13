06de2024a1235c63edb77c25efd31a37823ed297
Nicholas Hammond <nhammond129@gmail.com>
2021-09-12 23:50:54 -0500

    replace pygase

--- engine/client.py
+++ engine/client.py
@@ -1,15 +1,13 @@
-import pygase
+import socket
+import sys
 
-class Client(pygase.Client):
-	def __init__(self, *args, hostname='localhost', port=8080, **kwargs):
-		pygase.Client.__init__(self, *args, **kwargs)
-		self.connect_in_thread(hostname=hostname, port=port)
+HOST, PORT = "localhost", 8888
+data = " ".join(sys.argv[1:])
 
-		"""
-		all `on_X` functions will be bound to events with name `X`
-		"""
-		handlers = ['_'.join(fn.split('_')[1:]) for fn in dir(self) if fn.startswith('on_')]
-		event_handlers = {handler : getattr(self, 'on_'+handler) for handler in handlers}
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-		for event, handler in event_handlers.items():
-			self.register_event_handler(event, handler)
+sock.sendto(data + "\n", (HOST, PORT))
+received = sock.recv(1024)
+
+print("Sent:     {}".format(data))
+print("Received: {}".format(received))
--- engine/server.py
+++ engine/server.py
@@ -1,16 +1,20 @@
-import pygase
+import socketserver, threading, time
 
-class Server(pygase.Server):
-	database_file = 'data.db'
-	def __init__(self, *args, **kwargs):
-		pygase.Server.__init__(self, *args, **kwargs)
-		self.run_in_thread()
+class ThreadedUDPRequestHandler(socketserver.BaseRequestHandler):
 
-		"""
-		all `on_X` functions will be bound to events with name `X`
-		"""
-		handlers = ['_'.join(fn.split('_')[1:]) for fn in dir(self) if fn.startswith('on_')]
-		event_handlers = {handler : getattr(self, 'on_'+handler) for handler in handlers}
+    def handle(self):
+        data = self.request[0].strip()
+        socket = self.request[1]
+        current_thread = threading.current_thread()
+        print("{}: client: {}, wrote: {}".format(current_thread.name, self.client_address, data))
+        socket.sendto(data.upper(), self.client_address)
 
-		for event, handler in event_handlers.items():
-			self.register_event_handler(event, handler)
+class ThreadedUDPServer(socketserver.ThreadingMixIn, socketserver.UDPServer):
+    pass
+
+if __name__ == "__main__":
+    HOST, PORT = "0.0.0.0", 8888
+
+    server = ThreadedUDPServer((HOST, PORT), ThreadedUDPRequestHandler)
+    print(f"Server starting at {HOST}:{PORT}")
+    server.serve_forever()
