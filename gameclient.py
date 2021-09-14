7693a6f4059e61c6c642b3b89c116eab2292ee22
Nicholas Hammond <nhammond129@gmail.com>
2021-09-14 15:20:15 -0500

    Update gameclient.py

--- gameclient.py
+++ gameclient.py
@@ -1,41 +1,18 @@
-import pygase
-import time
+from engine.networking.sockets import ThreadedUDPSocket
 
-class GameClient(pygase.Client):
-	def __init__(self, *args, hostname='localhost', port=8080, **kwargs):
-		pygase.Client.__init__(self, *args, **kwargs)
-		self.connect_in_thread(hostname=hostname, port=port)
 
-		"""
-		all `on_X` functions will be bound to events with name `X`
-		"""
-		handlers = ['_'.join(fn.split('_')[1:]) for fn in dir(self) if fn.startswith('on_')]
-		event_handlers = {handler : getattr(self, 'on_'+handler) for handler in handlers}
 
-		for event, handler in event_handlers.items():
-			self.register_event_handler(event, handler)
-
-		while True:
-			with self.access_game_state() as game_state:
-				print(game_state.time_order, game_state.game_status)
-				if game_state.time_order > 0: break
-				try:
-					print(game_state.position)
-				except: pass
-		print("READY!")
+class Client(ThreadedUDPSocket):
+	def __init__(self, *args, **kwargs):
+		ThreadedUDPSocket.__init__(self, *args, **kwargs)
 	
-	def debug(self):
-		with self.access_game_state() as game_state:
-			# locks a copy of game state
-			print(game_state.position, game_state.hp)
-		self.dispatch_event("attack", attack_position=0.5)
+	def on_recv(self, addr, port, data):
+		print(f"{addr}:{port} | {data}")
 
-	def on_info(self, *args):
-		print(*args)
+def main():
+	client = Client()
+	while True:
+		client.think()
 
 if __name__ == "__main__":
-	client = GameClient()
-
-	while True:
-		client.debug()
-		time.sleep(1)
+	main()
