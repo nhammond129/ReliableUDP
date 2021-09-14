2f0fdf7f7450d65dcca62520c79a9a5cce9ef1d6
Nicholas Hammond <nhammond129@gmail.com>
2021-09-13 20:49:48 -0500

    aaaa

---
+++ engine/networking/connection.py
@@ -0,0 +1,27 @@
+import sockets
+
+class Connection:
+	def __init__(self, addr, port):
+		self.addr = addr
+		self.port = port
+
+class ClientConnection(Connection):
+	def __init__(self, addr, port, *args, **kwargs):
+		Connection
+
+class ServerConnection:
+	def __init__(self, addr, port):
+		self.addr = addr
+		self.port = port
+
+class Server(sockets.ThreadedUDPSocket):
+	def __init__(self, *args, **kwargs):
+		sockets.ThreadedUDPSocket.__init__(self, *args, **kwargs)
+		self.connections = []
+
+	def broadcast(self, data):
+		for conn in self.connections:
+			conn.send(data)
+	
+	def on_recv(self, addr, port, data):
+		self.sendto(data.upper(), (addr, port))
