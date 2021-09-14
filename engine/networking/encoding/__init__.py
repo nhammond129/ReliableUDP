cb16ff35da7eae391e3a923ff4f400ba92b34c2c
Nicholas Hammond <nhammond129@gmail.com>
2021-09-14 16:38:22 -0500

    relative import fix

--- engine/networking/encoding/__init__.py
+++ engine/networking/encoding/__init__.py
@@ -1,4 +1,4 @@
 # Styled after the libdiana encoding.py:
 # https://github.com/nhammond129/libdiana/blob/master/diana/encoding.py
-from encode import pack
-from decode import unpack
+from .encode import pack
+from .decode import unpack
