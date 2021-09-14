0d9bbb4fd2c581e0c13a163628bd4ffacdfa9679
Nicholas Hammond <nhammond129@gmail.com>
2021-09-14 16:38:28 -0500

    add test packets

--- engine/networking/packets.py
+++ engine/networking/packets.py
@@ -1,4 +1,4 @@
-import encoding
+import engine.networking.encoding as encoding
 PACKETS = {}
 
 def packet(id):
@@ -8,7 +8,35 @@ def packet(id):
 		return klass
 	return wrapper
 
-class Packet:
-	packet_id = 0
-	format = '!I'
-	
+@packet(0)
+class TestPacket:
+	format = 'i'
+	def __init__(self, test_number: int):
+		self.test_number = test_number
+
+	def encode(self):
+		return encoding.pack(self.format, self.test_number)
+
+	@classmethod
+	def decode(cls, data):
+		test_number, = encoding.unpack(cls.format, data)
+		return cls(test_number)
+
+	def __str__(self):
+		return f"<TestPacket test_number={self.test_number}>"
+@packet(1)
+class TestPacket2:
+	format = 'f'
+	def __init__(self, test_number: float):
+		self.test_number = test_number
+
+	def encode(self):
+		return encoding.pack(self.format, self.test_number)
+
+	@classmethod
+	def decode(cls, data):
+		test_number, = encoding.unpack(cls.format, data)
+		return cls(test_number)
+
+	def __str__(self):
+		return f"<TestPacket2 test_number={self.test_number}>"
