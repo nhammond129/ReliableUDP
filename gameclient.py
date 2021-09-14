5e2d1a68150aae06e21fbe2d5779a40aed0cc1eb
Nicholas Hammond <nhammond129@gmail.com>
2021-09-13 23:09:02 -0500

    Update gameclient.py

--- gameclient.py
+++ gameclient.py
@@ -1,9 +1,9 @@
-import engine.client
+import pygase
 import time
 
-class GameClient(engine.client.Client):
+class GameClient(pygase.Client):
 	def __init__(self, *args, hostname='localhost', port=8080, **kwargs):
-		engine.client.Client.__init__(self, *args, **kwargs)
+		pygase.Client.__init__(self, *args, **kwargs)
 		self.connect_in_thread(hostname=hostname, port=port)
 
 		"""
