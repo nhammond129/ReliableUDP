3f9ec99e0a752280d5b0615bae524015e8cb3df0
Nicholas Hammond <nhammond129@gmail.com>
2021-09-14 15:20:34 -0500

    connection.py initial

--- engine/networking/connection.py
+++ engine/networking/connection.py
@@ -1,28 +1,31 @@
-import sockets
-import bson
+from engine.networking.sockets import ThreadedUDPSocket
+import encoding
+import packets
 
-class Connection:
-	def __init__(self, addr, port):
-		self.addr = addr
-		self.port = port
-
-class ClientConnection(Connection):
-	def __init__(self, addr, port, *args, **kwargs):
-		Connection
-
-class ServerConnection:
-	def __init__(self, addr, port):
-		self.addr = addr
-		self.port = port
-
-class Server(sockets.ThreadedUDPSocket):
+class Connection(ThreadedUDPSocket):
+	"""
+	Packets will have the following structure:
+	[uint protocol id]
+	[uint sequence]
+	[uint ack]
+	[uint ack bitfields]
+	[uint payload length]
+	[ ... payload bytes ... ]
+	"""
+	protocol_id = 123180273
 	def __init__(self, *args, **kwargs):
-		sockets.ThreadedUDPSocket.__init__(self, *args, **kwargs)
-		self.connections = []
+		ThreadedUDPSocket.__init__(self, *args, **kwargs)
+		self.ack = 0
+		self.sequence = 0
 
-	def broadcast(self, data):
-		for conn in self.connections:
-			conn.send(data)
-	
-	def on_recv(self, addr, port, data):
-		self.sendto(data.upper(), (addr, port))
+	def send(self, packet: packets.Packet):
+		payload = packet.encode()
+		data = encoding.pack('IIIII*', )
+
+
+	def decode(data):
+		"""
+		returns (list: Packet, trailing)
+		"""
+		if not data: return [], b''
+		start_idx = data.find(self.protocol_id)
