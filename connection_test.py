a54dde64018bb0cfd024265bbc97525380ec7d3a
Nicholas Hammond <nhammond129@gmail.com>
2021-09-16 14:02:24 -0500

    tabs to spaces

--- connection_test.py
+++ connection_test.py
@@ -7,40 +7,40 @@ import time
 
 @packets.packet(0)
 class TestPacket:
-	format = 'i'
-	def __init__(self, test_number: int):
-		self.test_number = test_number
+    format = 'i'
+    def __init__(self, test_number: int):
+        self.test_number = test_number
 
-	def encode(self):
-		return pack(self.format, self.test_number)
+    def encode(self):
+        return pack(self.format, self.test_number)
 
-	@classmethod
-	def decode(cls, data):
-		test_number, = unpack(cls.format, data)
-		return cls(test_number)
+    @classmethod
+    def decode(cls, data):
+        test_number, = unpack(cls.format, data)
+        return cls(test_number)
 
-	def __str__(self):
-		return f"<TestPacket test_number={self.test_number}>"
+    def __str__(self):
+        return f"<TestPacket test_number={self.test_number}>"
 
     __repr__ = __str__
 
 @packets.packet(1)
 class TestPacket2:
-	format = 'f'
-	def __init__(self, test_number: float):
-		self.test_number = test_number
+    format = 'f'
+    def __init__(self, test_number: float):
+        self.test_number = test_number
 
-	def encode(self):
-		return pack(self.format, self.test_number)
+    def encode(self):
+        return pack(self.format, self.test_number)
 
-	@classmethod
-	def decode(cls, data):
-		test_number, = unpack(cls.format, data)
-		return cls(test_number)
+    @classmethod
+    def decode(cls, data):
+        test_number, = unpack(cls.format, data)
+        return cls(test_number)
 
-	def __str__(self):
-		return f"<TestPacket2 test_number={self.test_number}>"
-
+    def __str__(self):
+        return f"<TestPacket2 test_number={self.test_number}>"
+    
     __repr__ = __str__
 
 def main():
