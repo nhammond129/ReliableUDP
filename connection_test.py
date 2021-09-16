117a589d3a2399bf953e46ac6eff1aa7e17b9cb4
Nicholas Hammond <nhammond129@gmail.com>
2021-09-15 19:26:15 -0500

    move test packets location

--- connection_test.py
+++ connection_test.py
@@ -1,9 +1,43 @@
 from networking.connection import Connection, Client, Server
-from networking.packets import TestPacket, TestPacket2
+from networking.encoding import pack, unpack
+import networking.packets as packets
 
 import nulltk as tk
 import time
-import re
+
+@packets.packet(0)
+class TestPacket:
+	format = 'i'
+	def __init__(self, test_number: int):
+		self.test_number = test_number
+
+	def encode(self):
+		return pack(self.format, self.test_number)
+
+	@classmethod
+	def decode(cls, data):
+		test_number, = unpack(cls.format, data)
+		return cls(test_number)
+
+	def __str__(self):
+		return f"<TestPacket test_number={self.test_number}>"
+
+@packets.packet(1)
+class TestPacket2:
+	format = 'f'
+	def __init__(self, test_number: float):
+		self.test_number = test_number
+
+	def encode(self):
+		return pack(self.format, self.test_number)
+
+	@classmethod
+	def decode(cls, data):
+		test_number, = unpack(cls.format, data)
+		return cls(test_number)
+
+	def __str__(self):
+		return f"<TestPacket2 test_number={self.test_number}>"
 
 def main():
     server = Server('localhost', 8888)
--- networking/packets.py
+++ networking/packets.py
@@ -7,36 +7,3 @@ def packet(id):
 		klass.packet_id = id
 		return klass
 	return wrapper
-
-@packet(0)
-class TestPacket:
-	format = 'i'
-	def __init__(self, test_number: int):
-		self.test_number = test_number
-
-	def encode(self):
-		return pack(self.format, self.test_number)
-
-	@classmethod
-	def decode(cls, data):
-		test_number, = unpack(cls.format, data)
-		return cls(test_number)
-
-	def __str__(self):
-		return f"<TestPacket test_number={self.test_number}>"
-@packet(1)
-class TestPacket2:
-	format = 'f'
-	def __init__(self, test_number: float):
-		self.test_number = test_number
-
-	def encode(self):
-		return pack(self.format, self.test_number)
-
-	@classmethod
-	def decode(cls, data):
-		test_number, = unpack(cls.format, data)
-		return cls(test_number)
-
-	def __str__(self):
-		return f"<TestPacket2 test_number={self.test_number}>"
